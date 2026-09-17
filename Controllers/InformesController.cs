590776c14d7faa38adc27441d0c3e8e56864e0bd
GodoyDario <dariogodoy0896@gmail.com>
2026-09-17 16:30:53 -0300

    ABM de Inmuebles + 5 informes + proteccion login

--- Controllers/InformesController.cs
+++ Controllers/InformesController.cs
@@ -1,25 +1,27 @@
 using Inmobiliaria_DeborahGomez.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Inmobiliaria_DeborahGomez.Controllers;
 
-public class InformesController : Controller
+
+[Authorize] public class InformesController : Controller
 {
     private readonly IRepositorioInmueble repositorioInmueble;
+    private readonly IRepositorioPropietario repositorioPropietario;
 
-    public InformesController(IRepositorioInmueble repositorioInmueble)
+    public InformesController(IRepositorioInmueble repositorioInmueble, IRepositorioPropietario repositorioPropietario)
     {
         this.repositorioInmueble = repositorioInmueble;
+        this.repositorioPropietario = repositorioPropietario;
     }
 
-    // Menú principal de informes
     public IActionResult Index()
     {
         return View();
     }
 
-    // Informe 1: Listar inmuebles y su dueño, con filtro opcional por disponibilidad
-    
+    // Informe 1
     public IActionResult InmueblesPorDisponibilidad(bool? disponible, int pagina = 1)
     {
         var tamPagina = 10;
@@ -28,6 +30,89 @@ public class InformesController : Controller
 
         ViewBag.Disponible = disponible;
         ViewBag.Pagina = pagina;
+        ViewBag.TotalPaginas = (int)Math.Ceiling(cantidad / (double)tamPagina);
+
+        return View(lista);
+    }
+
+    // Informe 2
+    public IActionResult InmueblesPorPropietario(int? propietarioId, int pagina = 1)
+    {
+        var tamPagina = 10;
+
+        ViewBag.Propietarios = repositorioPropietario.ObtenerLista(null, 1, 1000);
+        ViewBag.PropietarioIdSeleccionado = propietarioId;
+        ViewBag.Pagina = pagina;
+
+        if (propietarioId == null)
+        {
+            ViewBag.TotalPaginas = 0;
+            return View(new List<Inmobiliaria_DeborahGomez.Models.Inmueble>());
+        }
+
+        var lista = repositorioInmueble.ObtenerPorPropietario(propietarioId.Value, pagina, tamPagina);
+        var cantidad = repositorioInmueble.ObtenerCantidadPorPropietario(propietarioId.Value);
+
+        ViewBag.TotalPaginas = (int)Math.Ceiling(cantidad / (double)tamPagina);
+
+        return View(lista);
+    }
+
+    // Informe 3
+    public IActionResult InmueblesMasReservados(int dias = 365, int pagina = 1)
+    {
+        var tamPagina = 10;
+
+        var lista = repositorioInmueble.ObtenerMasReservados(dias, pagina, tamPagina);
+        var cantidad = repositorioInmueble.ObtenerCantidadMasReservados(dias);
+
+        ViewBag.Dias = dias;
+        ViewBag.Pagina = pagina;
+        ViewBag.TotalPaginas = (int)Math.Ceiling(cantidad / (double)tamPagina);
+
+        return View(lista);
+    }
+
+    // Informe 4
+    public IActionResult InmueblesSinReservas(int dias = 30, int pagina = 1)
+    {
+        var tamPagina = 10;
+
+        var lista = repositorioInmueble.ObtenerSinReservas(dias, pagina, tamPagina);
+        var cantidad = repositorioInmueble.ObtenerCantidadSinReservas(dias);
+
+        ViewBag.Dias = dias;
+        ViewBag.Pagina = pagina;
+        ViewBag.TotalPaginas = (int)Math.Ceiling(cantidad / (double)tamPagina);
+
+        return View(lista);
+    }
+
+    // Informe 5: Inmuebles disponibles (no ocupados) entre dos fechas
+    public IActionResult InmueblesDisponiblesEntreFechas(DateTime? fechaDesde, DateTime? fechaHasta, int pagina = 1)
+    {
+        var tamPagina = 10;
+
+        ViewBag.FechaDesde = fechaDesde;
+        ViewBag.FechaHasta = fechaHasta;
+        ViewBag.Pagina = pagina;
+
+        if (fechaDesde == null || fechaHasta == null)
+        {
+            ViewBag.TotalPaginas = 0;
+            return View(new List<Inmobiliaria_DeborahGomez.Models.Inmueble>());
+        }
+
+        if (fechaDesde >= fechaHasta)
+        {
+            ModelState.AddModelError("", "La fecha desde debe ser anterior a la fecha hasta.");
+            ViewBag.TotalPaginas = 0;
+            return View(new List<Inmobiliaria_DeborahGomez.Models.Inmueble>());
+        }
+
+        var lista = repositorioInmueble.ObtenerDisponiblesEntreFechas(fechaDesde.Value, fechaHasta.Value, pagina, tamPagina);
+        var cantidad = repositorioInmueble.ObtenerCantidadDisponiblesEntreFechas(fechaDesde.Value, fechaHasta.Value);
+
         ViewBag.TotalPaginas = (int)Math.Ceiling(cantidad / (double)tamPagina);
 
         return View(lista);
--- Repositories/IRepositorioInmueble.cs
+++ Repositories/IRepositorioInmueble.cs
@@ -7,4 +7,19 @@ public interface IRepositorioInmueble : IRepositorio<Inmueble>
     // Listar inmuebles y su dueño, con filtro opcional por disponibilidad
     IList<Inmueble> ObtenerPorDisponibilidad(bool? disponible, int pagina = 1, int tamPagina = 10);
     int ObtenerCantidadPorDisponibilidad(bool? disponible);
+    // Informe 2: Listar inmuebles de un propietario específico
+    IList<Inmueble> ObtenerPorPropietario(int propietarioId, int pagina = 1, int tamPagina = 10);
+    int ObtenerCantidadPorPropietario(int propietarioId);
+
+    // Informe 3: Inmuebles más reservados en los últimos X días (default 365)
+    IList<InmuebleConReservas> ObtenerMasReservados(int dias = 365, int pagina = 1, int tamPagina = 10);
+    int ObtenerCantidadMasReservados(int dias = 365);
+
+    // Informe 4: Inmuebles SIN reservas en los últimos X días
+    IList<Inmueble> ObtenerSinReservas(int dias, int pagina = 1, int tamPagina = 10);
+    int ObtenerCantidadSinReservas(int dias);
+
+    // Informe 5: Inmuebles NO ocupados (disponibles) entre dos fechas dadas
+    IList<Inmueble> ObtenerDisponiblesEntreFechas(DateTime fechaDesde, DateTime fechaHasta, int pagina = 1, int tamPagina = 10);
+    int ObtenerCantidadDisponiblesEntreFechas(DateTime fechaDesde, DateTime fechaHasta);
 }
